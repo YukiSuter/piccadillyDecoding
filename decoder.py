63cbcefaa7732980d4c4c705bfecfb64f37834b9
YukiSuter <yukisuter@ycsuter.dev>
2024-05-02 18:24:26 +0100

    stuff
    stuff

--- decoder.py
+++ decoder.py
@@ -1,7 +1,24 @@
-with open("copy file_truncated.pcc", "rb") as f:
+import wave
+
+with open("HT23_TRIM.raw", "rb") as f:
     content = f.read().hex()
 
 thehex = content[0x0041b200*2:0x0041b2FF*2]
 
 print(len(thehex))
+print(content)
 print(bytearray.fromhex(thehex).decode())
+
+out = wave.open(open("HT23_TRIM.wav", "wb"))
+
+out.setnchannels(1)
+out.setsampwidth(1)
+out.setframerate(4000)
+
+for i in range(1, len(content), 2):
+    if content[i-1:i+1] != "24" and content[i-2:i] != "24":
+        # print("iffed")
+        # print(content[i-1:i+1])
+        out.writeframes(bytes([(int("0x"+content[i-1:i+1], 0))&0x40>>1]))
+    else:
+        out.writeframes(bytes([(0)]))
--- script.py
+++ script.py
@@ -1,18 +1,12 @@
 import wave
 
-data = open("alarm.pcc", "rb").read()
-rawout = open("alarmraw.pcc", "wb")
-out = wave.open(open("alarm.wav", "wb"))
+data = open("HT23.raw", "rb").read()
+out = wave.open(open("HT23.wav", "wb"))
 
 out.setnchannels(1)
 out.setsampwidth(1)
 out.setframerate(4000)
 
 out.writeframes(bytes([
-    (data[i]&0x40)<<1 for i in range(1, len(data), 2)
-]))
-
-
-rawout.write(bytes([
     (data[i]&0x40)<<1 for i in range(1, len(data), 2)
 ]))
